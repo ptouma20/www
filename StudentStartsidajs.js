4f319438d47e294fc6e8df6d20e78aae8af55459
DEGGJUR <38204930+DEGGJUR@users.noreply.github.com>
2018-05-18 13:32:41 +0200

    d
    d

--- StudentStartsidajs.js
+++ StudentStartsidajs.js
@@ -1,38 +1,35 @@
-// function sok(string, sokarray) {
-//     // var Fuse = require('fuse.js');
-//      //var jsVar = require('xjobblista.php')
+function sok(string, sokarray) {
+    // var Fuse = require('fuse.js');
+     //var jsVar = require('xjobblista.php')
      
-//      var options = {
-//        shouldSort: true,
-//        tokenize: true,
-//        matchAllTokens: true,
-//        threshold: 0.5,
-//        location: 0,
-//        distance: 1000,
-//        maxPatternLength: 32,
-//        minMatchCharLength: 2,
-//        keys: [
-//          "ID",
-//          "name",
-//          "webpage",
-//          "email"
-//        ]
-//      };
+     var options = {
+       shouldSort: true,
+       tokenize: true,
+       matchAllTokens: true,
+       threshold: 0.5,
+       location: 0,
+       distance: 1000,
+       maxPatternLength: 32,
+       minMatchCharLength: 2,
+       keys: [
+         "description"
+       ]
+     };
   
-//      var langden = String(string);
+     var langden = String(string);
   
-//      if(langden.length < 5){
-//        options.threshold = 0;
-//      }
-//      else{
-//        options.threshold = 0.5;
-//      }
+     if(langden.length < 3){
+       options.threshold = 0;
+     }
+     else{
+       options.threshold = 0.5;
+     }
      
-//      var fuse = new Fuse(sokarray, options); // "list" is the item array
+     var fuse = new Fuse(sokarray, options); // "list" is the item array
    
-//      var result = fuse.search(string);
-//      //console.log(result);
+     var result = fuse.search(string);
+     //console.log(result);
      
-//      return result;
-//    }
+     return result;
+   }
    
