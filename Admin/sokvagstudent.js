2d2ea8c0e0a171973cb7198fe1d504f259163b64
FredrikLidstrom <38204694+FredrikLidstrom@users.noreply.github.com>
2018-05-29 09:47:26 +0200

    dasd
    ada

---
+++ Admin/sokvagstudent.js
@@ -0,0 +1,37 @@
+function Studentsok(string, sokarray) {
+  // var Fuse = require('fuse.js');
+   //var jsVar = require('xjobblista.php')
+   
+   var options = {
+     shouldSort: true,
+     tokenize: true,
+     matchAllTokens: true,
+     threshold: 0.5,
+     location: 0,
+     distance: 1000,
+     maxPatternLength: 32,
+     minMatchCharLength: 2,
+     keys: [
+       "Username",
+       "Bio",
+       "Education"
+     ]
+   };
+
+   var langden = String(string);
+
+   if(langden.length < 3){
+     options.threshold = 0;
+   }
+   else{
+     options.threshold = 0.5;
+   }
+   
+   var fuse = new Fuse(sokarray, options); // "list" is the item array
+ 
+   var result = fuse.search(string);
+   //console.log(result);
+   
+   return result;
+ }
+ 
